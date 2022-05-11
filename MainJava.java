b806a83950876c5e5449524f12400e852584e4f4
Technically-Insane <86095028+Technically-Insane@users.noreply.github.com>
2022-05-11 18:17:09 -0400

    Java / Python Tutorials

---
+++ MainJava.java
@@ -0,0 +1,24 @@
+class main {// The class is needed for code to be read
+
+    public static void main(String[] args) {// Whatever is in the main runs
+
+        // Basic Syntax
+        int Integer = 10;
+        double Decimal = 10.9;
+        String Text = "This is Text";
+        char Character = '$';
+        boolean TrueFalse = true;
+
+        if (Integer > 10 || Decimal > 10) {
+            Text = "This is an If/Else Statement";
+        } else {
+            Text = "Variables can be updated";
+        }
+
+        while (Integer == 10) {
+            Text = "While Statements repeat as long as the condition is true";
+        }
+
+        System.out.println("This Outputs Text");
+    }
+}
---
+++ MainPython.py
@@ -0,0 +1,18 @@
+# Basic Syntax
+Integer = 10
+Decimal = 10.9
+Text = "This is Text"
+Character = '$'
+TrueFalse = True
+
+if (Integer > 10 and Decimal > 10):
+    Text = "This is an If/Else Statement"
+else:
+    Text = "Variables can be updated"
+
+
+while (Integer == 10):
+    Text = "While Statements repeat as long as the condition is true"
+
+
+print("This Outputs Text")
