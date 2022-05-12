dce082e28e4bdbf2bbeffe9eef5b43fb44b6d264
Technically-Insane <86095028+Technically-Insane@users.noreply.github.com>
2022-05-11 21:09:37 -0400

    Added C#

---
+++ C#/MainC#.cs
@@ -0,0 +1,33 @@
+using System;//Clarifies System
+
+namespace MainNamespace//Namespace works the same as a Java Class, but broader
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Integer = 10;
+            Decimal = 10.9;
+            Text = "This is text";
+            Character = '$';
+            TrueFalse  = false;
+
+            if (Integer == 10 && Decimal > 10.9)
+            {
+                Text = "This is an If / Else Statement";
+            }
+            else
+            {
+                Text = "Variables can be updates";
+            }
+
+            while (Integer == 10)
+            {
+                Text = "While Statements repeat as long as the condition is true";
+            }
+
+            Console.WriteLine"This outputs text";
+        }
+    }
+}
+
