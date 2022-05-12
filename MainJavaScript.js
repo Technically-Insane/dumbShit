bcd6a27f2e9ba7dc5d1a3daf366bef8d50816b95
Technically-Insane <86095028+Technically-Insane@users.noreply.github.com>
2022-05-11 20:44:22 -0400

    Added HTML and JavaScript

---
+++ MainJavaScript.js
@@ -0,0 +1,18 @@
+var Integer = 10;
+let Decimal = 10.9;
+var Text = "This is text";
+let Character = "$";
+var TrueFalse = false;
+
+if (Integer == 10 && Decimal > 10.9) {
+    Text = "This is an If / Else Statement";
+}
+else {
+    Text = "Variables can be updates";
+}
+
+while (Integer == 10) {
+    Text = "While Statements repeat as long as the condition is true";
+}
+
+print("This outputs text");
--- playground_molina.java
+++ playground_molina.java
@@ -1,4 +1,4 @@
-class main {
+class mainClass {
     public static void main(String[] args) {
         System.out.println("sss");
     }
